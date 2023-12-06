6fec884807907ea7bd7d99c6f22122d474608830
cvipenta <cvipenta@gmail.com>
2023-12-06 02:11:40 +0200

    Expo tutorial - take a screenshot

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, View, Platform} from 'react-native';
 import {useRef, useState} from "react";
 import * as ImagePicker from 'expo-image-picker';
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import * as MediaLibrary from "expo-media-library";
-import { captureRef } from 'react-native-view-shot';
+import {captureRef} from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
@@ -50,18 +51,35 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("Saved!");
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
@@ -99,10 +117,11 @@ export default function App() {
         )}
 
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
       </EmojiPicker>
 
-      <StatusBar style="auto"/>
+      <StatusBar style="light"/>
+
     </GestureHandlerRootView>
   );
 }
